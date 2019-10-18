93269d2744609b2d8dadd65491d9d4a953eed80b
Sarah Elzinga <sarah.elzinga@flatironschool.com>
2019-10-18 14:35:32 -0500

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[5.2]
-end
+    def change
+      create_table :songs do |t|
+        t.string :name 
+        t.integer :artist_id
+        t.integer :genre_id
+      end
+    end
+  end
