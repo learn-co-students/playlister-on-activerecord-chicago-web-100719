e83f0d90dd90e5822f159a6e5aa7ff5cfabfafa9
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2019-10-21 10:12:43 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,18 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
+  end
+
 end
